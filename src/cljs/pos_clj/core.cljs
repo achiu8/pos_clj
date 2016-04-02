2102b28ad17eeac2f9d185ecc8df42bb3517421a
kprakobkit <kprakobkit@gmail.com>
2016-04-02 16:51:28 -0700

    Lists all orders.

--- src/cljs/pos_clj/core.cljs
+++ src/cljs/pos_clj/core.cljs
@@ -1,16 +1,20 @@
 (ns pos-clj.core
   (:require [om.core :as om :include-macros true]
-            [om.dom :as dom :include-macros true]))
+            [om.dom :as dom :include-macros true]
+            [sablono.core :as html :refer-macros [html]]))
 
 (enable-console-print!)
 
-(defonce app-state (atom {:text "POS CLJ"}))
+(defonce app-state (atom {:orders [{:table-number 1 :status "OPEN"} {:table-number 2 :status "CLOSE"}]}))
 
 (defn root-component [app owner]
   (reify
     om/IRender
     (render [_]
-      (dom/div nil (dom/h1 nil (:text app))))))
+      (html
+       [:div
+        [:h1 "Orders"]
+        [:div (map (fn [order] [:div (str "order: " (:table-number order))]) (:orders app))]]))))
 
 (om/root
  root-component
